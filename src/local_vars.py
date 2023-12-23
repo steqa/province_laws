95f1bb461eeddb8ac95029698bdaceba3fb3de06
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-12-23 22:41:35 +0700

    Initial
    1. Created the django project
    2. Setup the static storage
    3. Setup the database

---
+++ src/local_vars.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'django-insecure-7av_n_8_g_14&nplz+oabd#@fsb!76ekiwrv*=-gt^)d61vi0!'
+
+DB_NAME = 'postgres'
+DB_USER = 'postgres'
+DB_PASSWORD = 'ckArwwvlHB5638'
+DB_HOST = 'db.hqnjkcheohnmsceswnlg.supabase.co'
+DB_PORT = '5432'
+
+AWS_S3_ENDPOINT_URL = 'hb.ru-msk.vkcs.cloud'
+AWS_S3_ACCESS_KEY_ID = 'jdLRtoeXk3AuLKVUCGNP5p'
+AWS_S3_SECRET_ACCESS_KEY = 'bKurxJKVacvWVTPifTvghDLoJtjKVsLwNEjnHrRwNGag'
